b3b6c807486b8d13c01b722da4a53726a5830e72
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-19 17:53:52 -0300

    :bike:

--- remove_tokens.sh
+++
@@ -1,3 +0,0 @@
-sed -ie "s/Bearer .*/Bearer token/g" tests/cassettes/github_requests.yaml
-sed -ie "s/gh[a-z]_[A-Za-z0-9]*/token/g" tests/cassettes/github_requests.yaml
-rm tests/cassettes/github_requests.yamle
