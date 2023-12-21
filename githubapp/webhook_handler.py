68f0741d61d92d75b4956df5118c0d116a86ce21
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-21 14:56:11 -0300

    webhook handler docs

--- githubapp/webhook_handler.py
+++ githubapp/webhook_handler.py
@@ -17,6 +17,17 @@ class SignatureError(Exception):
 
 
 def webhook_handler(event: type[Event]):
+    """Decorator to register a method as a webhook handler.
+
+    The method must accept only one argument of the Event type.
+
+    Args:
+        event: The event type to handle.
+
+    Returns:
+        A decorator that registers the method as a webhook handler.
+    """
+
     def decorator(method):
         WebhookHandler.add_handler(event, method)
         return method
@@ -25,33 +36,62 @@ def webhook_handler(event: type[Event]):
 
 
 class WebhookHandler:
+    """Class to handle webhook requests.
+
+    The WebhookHandler class provides a method to register webhook handlers and a method to handle webhook requests.
+
+    Attributes:
+        handlers: A dictionary of event types to lists of handlers.
+    """
+
     handlers = defaultdict(list)
 
     @staticmethod
     def add_handler(event: type[Event], method: Callable):
+        """Add a handler for a specific event type.
+
+        The handler must accept only one argument of the Event type.
+
+        Args:
+            event: The event type to handle.
+            method: The handler method.
+        """
         if subclasses := event.__subclasses__():
             for sub_event in subclasses:
                 WebhookHandler.add_handler(sub_event, method)
         else:
+            WebhookHandler._validate_signature(method)
             WebhookHandler.handlers[event].append(method)
 
     @staticmethod
     def handle(headers: dict[str, Any], body: dict[str, Any]):
+        """Handle a webhook request.
+
+        The request headers and body are passed to the appropriate handler methods.
+
+        Args:
+            headers: The request headers.
+            body: The request body.
+        """
+
         event_class = Event.get_event(headers, body)
-        # event = headers["X-Github-Event"]
-        # action = body.pop("action", None)
-        #
-        # event_class = next(filter(lambda e: e.event == event, Event.__subclasses__()))
-        #
-        # event_class = next(
-        #     filter(lambda x: x.action == action, event_class.__subclasses__()), None
-        # )
         body.pop("action", None)
         for handler in WebhookHandler.handlers.get(event_class, []):
             handler(event_class(headers, **body))
 
     @staticmethod
     def root(name):
+        """Decorator to register a method as the root handler.
+
+        The root handler is called when no other handler is found for the request.
+
+        Args:
+            name: The name of the root handler.
+
+        Returns:
+            A decorator that registers the method as the root handler.
+        """
+
         def root_wrapper():
             return f"{name} App up and running!"
 
@@ -59,15 +99,20 @@ class WebhookHandler:
 
     @staticmethod
     def _validate_signature(method):
+        """Validate the signature of a webhook handler method.
+
+        The method must accept only one argument of the Event type.
+
+        Args:
+            method: The method to validate.
+
+        Raises:
+            SignatureError: If the method has a wrong signature.
+        """
+
         parameters = inspect.signature(method).parameters
         try:
             assert len(parameters) == 1
         except AssertionError:
             signature = ""
             raise SignatureError(method, signature)
-
-    # def webhook(self):
-    #     body = request.json
-    #     headers = dict(request.headers)
-    #     self.handle_webhook(headers, body)
-    #     return "OK"
