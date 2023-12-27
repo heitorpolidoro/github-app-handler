90668bb3d0f59fec8dc604a092a56f10c7314989
Heitor Polidoro <heitor.polidoro@gmail.com>
2023-12-27 18:55:18 -0300

    chore(Sonar): complexity

--- tests/test_event.py
+++ tests/test_event.py
@@ -87,20 +87,17 @@ def test_all_events(event_action_request):
         headers["X-Github-Event"] = event_class.event_identifier["event"]
         if subclasses := event_class.__subclasses__():
             for sub_event_class in subclasses:
-                body.update(sub_event_class.event_identifier)
-                event = Event.get_event(headers, body)
-                assert event == sub_event_class
-                for attr in inspect.signature(event).parameters:
-                    if attr != "headers":
-                        body[attr] = "value"
-
-                event(headers, **body)
+                instantiate_class(body, headers, sub_event_class)
         else:
-            body.update(event_class.event_identifier)
-            event = Event.get_event(headers, body)
-            assert event == event_class
-            for attr in inspect.signature(event).parameters:
-                if attr != "headers":
-                    body[attr] = "value"
+            instantiate_class(body, headers, event_class)
 
-            event(headers, **body)
+
+def instantiate_class(body, headers, clazz):
+    """ Instantiate and validate an event or sub event class"""
+    body.update(clazz.event_identifier)
+    event = Event.get_event(headers, body)
+    assert event == clazz
+    for attr in inspect.signature(event).parameters:
+        if attr != "headers":
+            body[attr] = "value"
+    event(headers, **body)
