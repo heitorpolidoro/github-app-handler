0e761919ab53b8f67711312cc17a441fe72bbac0
Heitor Polidoro <heitor.polidoro@gmail.com>
2024-01-04 17:31:04 -0300

    check_run module docstring

--- githubapp/events/check_run.py
+++ githubapp/events/check_run.py
@@ -1,3 +1,7 @@
+"""
+Module for handling GitHub check_run webhook events.
+https://docs.github.com/en/webhooks/webhook-events-and-payloads#check_run
+"""
 from github.CheckRun import CheckRun
 from github.NamedUser import NamedUser
 from github.Repository import Repository
@@ -7,7 +11,7 @@ from githubapp.LazyCompletableGithubObject import LazyCompletableGithubObject
 
 
 class CheckRunEvent(Event):
-    """This class represents an issue event."""
+    """This class represents an check run event."""
 
     event_identifier = {"event": "check_run"}
 
@@ -32,6 +36,6 @@ class CheckRunEvent(Event):
 
 
 class CheckRunCompletedEvent(CheckRunEvent):
-    """This class represents an issue event."""
+    """This class represents an check run completed event."""
 
     event_identifier = {"action": "completed"}
