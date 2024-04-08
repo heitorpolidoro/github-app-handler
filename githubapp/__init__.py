55eb185d4568f7137be04da97b05b217fde58d87
Heitor Polidoro <heitor.polidoro@gmail.com>
2024-04-08 13:32:32 -0300

    [release:bugfix]

--- githubapp/__init__.py
+++ githubapp/__init__.py
@@ -7,6 +7,6 @@ the necessary components and configurations. Any global settings for the package
 
 from githubapp.config import Config
 
-__version__ = "0.24.1"
+__version__ = "0.24.4"
 
 __all__ = ["Config"]
